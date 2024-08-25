2a19eabadc824e03772de052bb05d00ca81f316e
DiegoDexx <diegojspro@gmail.com>
2024-08-25 13:53:41 +0200

    changes11_admin

--- src/components/bookingModal.jsx
+++ src/components/bookingModal.jsx
@@ -124,7 +124,7 @@ const BookingModal = ({ setShowBookingModal }) => {
             flex-direction: column;
             align-items: center;
             justify-content: center;
-            padding: 20px;
+            padding: 0;
             background-color: #f8f9fa;
             border-radius: 10px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
