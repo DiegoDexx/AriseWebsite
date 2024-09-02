2d0e37dbc02e60f1833c5bc881f4e9dca4e928d7
DiegoDexx <diegojspro@gmail.com>
2024-09-02 11:42:28 +0200

    banner2

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import banner_background from '../assets/img/banner_background.png';
+import banner_background from '../assets/img/banner_background2.png';
 
 const Countdown = () => {
   
