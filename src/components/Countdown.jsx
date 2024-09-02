244c5bc916bfca3d3bc9acbaaf73046cc6d0fb75
DiegoDexx <diegojspro@gmail.com>
2024-09-02 02:34:11 +0200

    banner1

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import banner_backround from '../assets/img/banner_backround.png';
 
 const Countdown = () => {
   const calculateTimeLeft = (targetDate) => {
